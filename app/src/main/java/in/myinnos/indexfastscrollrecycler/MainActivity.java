cbd72eed3f2a724b48060e682250ad7d2d7207a6
myinnos <prabhakar_thota@live.com>
2017-07-20 13:41:11 +0530

    Highlighting of the Alphabet indexers

--- app/src/main/java/in/myinnos/indexfastscrollrecycler/MainActivity.java
+++ app/src/main/java/in/myinnos/indexfastscrollrecycler/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setIndexbarWidth(40);
         mRecyclerView.setPreviewPadding(0);
         mRecyclerView.setIndexBarTextColor("#FFFFFF");
+
         mRecyclerView.setIndexBarVisibility(true);
         mRecyclerView.setIndexbarHighLateTextColor("#33334c");
         mRecyclerView.setIndexBarHighLateTextVisibility(true);
